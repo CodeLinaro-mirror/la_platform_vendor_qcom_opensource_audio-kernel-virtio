6a54fd5e88414d83d2e6c1c85672fd2257503455
Susan Wang <quic_yingshuw@quicinc.com>
2023-10-16 18:34:34 -0400

    audio-kernel-virtio: Build VIO snd ko only for GY projects
    - Narrow down target list to build the VIO snd ko
      only for HGY projects
    
    Change-Id: I599f7d719af50e61e27a49ab7979080d883fd800

--- audio_kernel_virtio_product_board.mk
+++ audio_kernel_virtio_product_board.mk
@@ -1,7 +1,5 @@
 # Build audio kernel driver
 
-ifeq ($(call is-board-platform-in-list,msmnile gen4), true)
-ifneq (,$(filter $(TARGET_BOARD_PLATFORM)$(TARGET_BOARD_SUFFIX), msmnile_gvmq gen4_gvm))
+ifeq ($(TARGET_USES_GY), true)
 PRODUCT_PACKAGES  += $(KERNEL_MODULES_OUT)/msm_virtio_snd.ko
-endif
 endif
